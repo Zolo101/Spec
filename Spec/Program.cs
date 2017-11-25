bc9529119eca49811aba15d445d42864edf436b8
Zolo101 <umadeguy@gmail.com>
2017-11-25 13:56:32 +0000

    dont touch the .exe
    im warning you

--- Spec/Program.cs
+++ Spec/Program.cs
@@ -3,11 +3,30 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using RogueSharp;
+using RLNET;
 
 namespace Spec
 {
     class Program
     {
+        private static readonly int _Width = 100;
+        private static readonly int _Height = 70;
+        private static RLRootConsole _rootConsole;
+
+        private static readonly int _mapWidth = 100;
+        private static readonly int _mapHeight = 56;
+        private static RLConsole _mapConsole;
+
+        private static readonly int _messageWidth = 100;
+        private static readonly int _messageHeight = 10;
+        private static RLConsole _messageConsole;
+
+        private static readonly int _titleWidth = 100;
+        private static readonly int _titleHeight = 4;
+        private static RLConsole _titleConsole;
+        // console
+        
         static void RC(ConsoleColor back, ConsoleColor fore) // RC(ConsoleColor.,ConsoleColor.);
         {
             Console.BackgroundColor = back;
@@ -34,10 +53,11 @@ namespace Spec
                 Console.WriteLine("");
             }
         }
-        static void menu()
+        static void Menu()
         {
             bool menu = true;
             int ms = 2;
+            var input = Console.ReadKey();
             while (menu == true)
             {
                 Console.WriteLine("                                      ▄████████    ▄███████▄    ▄████████  ▄████████");
@@ -47,11 +67,11 @@ namespace Spec
                 Console.WriteLine("                                    ▀██████████ ▀█████████▀  ▀▀███▀▀▀     ███       ");
                 Console.WriteLine("                                            ███   ███          ███    █▄  ███    █▄ ");
                 Console.WriteLine("                                      ▄█    ███   ███          ███    ███ ███    ███");
-                Console.WriteLine("                                    ▄████████▀   ▄████▀        ██████████ ████████▀   0.12");
+                Console.WriteLine("                                    ▄████████▀   ▄████▀        ██████████ ████████▀  0.13");
                 Space(36);
                 RC(ConsoleColor.Red, ConsoleColor.Black);
-                Console.Write("Now with more semi-colons");
-                Console.WriteLine(ms);
+                Console.Write("The launcher");
+                Console.Write(ms);
                 RC(ConsoleColor.Black, ConsoleColor.White);
                 Console.WriteLine("");
                 if (ms == 1) RC(ConsoleColor.White, ConsoleColor.Black); else Console.ResetColor();
@@ -59,76 +79,75 @@ namespace Spec
                 if (ms == 2) RC(ConsoleColor.White, ConsoleColor.Black); else Console.ResetColor();
                 Console.WriteLine("Credits");
                 Console.ResetColor();
-                EmptyLine(4); Console.Write("Type "); RC(ConsoleColor.Black, ConsoleColor.Green); Console.Write("G"); Console.ResetColor(); Console.Write(" three times to click a button.");
-                if (ms <= 2)
+                EmptyLine(2); Console.Write("Type "); RC(ConsoleColor.Black, ConsoleColor.Green); Console.Write("W"); Console.ResetColor(); Console.Write(" four times to go up.");
+                EmptyLine(1); Console.Write("Type "); RC(ConsoleColor.Black, ConsoleColor.Green); Console.Write("S"); Console.ResetColor(); Console.Write(" four times to go down.");
+                EmptyLine(1); Console.Write("Type "); RC(ConsoleColor.Black, ConsoleColor.Green); Console.Write("G"); Console.ResetColor(); Console.Write(" four times to click a button.");
+                if (Console.ReadKey().Key == ConsoleKey.W)
                 {
-                    if (ms >= 1)
-                    {
-                        if (Console.ReadKey().Key == ConsoleKey.W)
-                        {
-                            ms = ms - 1;
-                        }
-                        if (Console.ReadKey().Key == ConsoleKey.S)
-                        {
-                            ms = ms + 1;
-                        }
-                        if (Console.ReadKey().Key == ConsoleKey.G & ms == 1)
-                        {
-                            menu = false;
-                            Console.Clear();
-                            Setup();
-                        }
-                        if (Console.ReadKey().Key == ConsoleKey.G & ms == 2)
-                        {
-                            menu = false;
-                            Console.Clear();
-                            Credits();
-                        } 
-                    }
+                    ms = ms - 1;
+                }
+                if (Console.ReadKey().Key == ConsoleKey.S)
+                {
+                    ms = ms + 1;
+                }
+                if (Console.ReadKey().Key == ConsoleKey.G & ms == 1)
+                {
+                    menu = false;
+                    Console.Clear();
+                    Draw();
+                }
+                if (Console.ReadKey().Key == ConsoleKey.G & ms == 2)
+                {
+                    menu = false;
+                    Console.Clear();
+                    Credits();
                 }
                 Console.Clear();
             }
         }
-        static void Setup()
+        static void Draw()
         {
-            string name = "NaN";
-            string world = "NaN";
-            RC(ConsoleColor.Blue, ConsoleColor.Black); Console.WriteLine("What is your atom's name?"); Console.ResetColor(); name = Console.ReadLine();
-            RC(ConsoleColor.Blue, ConsoleColor.Black); Console.WriteLine("What name would you like your world to be called?"); Console.ResetColor(); world = Console.ReadLine();
-            Draw(name, world);
+            string font = "terminal8x8.png"; // font
+            string title = "Spec"; // title
+
+            _rootConsole = new RLRootConsole(font, _Width, _Height,8,8,1f,title); // console
+            _mapConsole = new RLConsole(_mapWidth, _mapHeight);
+            _messageConsole = new RLConsole(_messageWidth, _messageHeight);
+            _titleConsole = new RLConsole(_titleWidth, _titleHeight);
+
+            _rootConsole.Update += OnRootConsoleUpdate;
+            _rootConsole.Render += OnRootConsoleRender;
+            // game loop
+            _rootConsole.Run();
         }
-        static void Draw(string name, string world)
+        private static void OnRootConsoleUpdate(object sender, UpdateEventArgs e)
         {
-            bool game = true;
-            int i = 0; // map
-            int x = 0; int y = 0;
-            int[] worldmap = {0,1,1,1,1,1,0,
-                              0,0,0,1,0,0,0,
-                              1,0,1,0,1,1,0,
-                              1,1,1,1,1,1,0,
-                              0,0,0,0,0,0,0,
-                              0,0,0,0,1,0,1,
-                              1,0,1,0,1,0,0};
-            while (game == true)
-            {
-                Console.Clear();
-                Console.WriteLine(name);
-                Console.WriteLine(world);
-                EmptyLine(2);
-                for (i = 0; i < 49; i++ )
-                {
-                    Console.Write("");
-                }
-                Console.ReadLine(); 
-            }
-        }
+            _mapConsole.SetBackColor(0, 0, _mapWidth, _mapHeight, RLColor.Black);
+            _mapConsole.Print(1, 1, "Map", RLColor.White);
+            _messageConsole.SetBackColor(0, 0, _messageWidth, _messageHeight, RLColor.Gray);
+            _messageConsole.Print(1, 1, "Messages", RLColor.White);
+            _titleConsole.SetBackColor(0, 0, _titleWidth, _titleHeight, RLColor.LightGray);
+            _titleConsole.Print(1, 1, "Spec 0.13", RLColor.White);
+        } // update
+        private static void OnRootConsoleRender(object sender, UpdateEventArgs e)
+        {
+            RLConsole.Blit(_titleConsole, 0, 0, _titleWidth, _titleHeight,
+              _rootConsole, 0, 0);
+            RLConsole.Blit(_mapConsole, 0, 0, _mapWidth, _mapHeight,
+              _rootConsole, 0, _titleHeight);
+            RLConsole.Blit(_messageConsole, 0, 0, _messageWidth, _messageHeight,
+              _rootConsole, 0, _Height - _messageHeight);
+
+            _rootConsole.Draw();
+        } // render
         static void Credits()
         {
-            Console.WriteLine("this was made by your boi zelo/zolo101."); Console.WriteLine("this will go back to the menu in 4 seconds."); System.Threading.Thread.Sleep(4000); Console.Clear(); menu();
+            Console.WriteLine("this was made by your boi zelo/zolo101."); Console.WriteLine("this will go back to the menu in 4 seconds."); System.Threading.Thread.Sleep(4000); Console.Clear(); Menu();
         }
         static void Main(string[] args)
         {
-            menu();
+            Console.Title = "Spec Launcher";
+            Menu();
             Console.ReadKey();
         }
     }
--- Spec/Properties/AssemblyInfo.cs
+++ Spec/Properties/AssemblyInfo.cs
@@ -6,7 +6,7 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("Spec")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("make yourself")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("Spec")]
